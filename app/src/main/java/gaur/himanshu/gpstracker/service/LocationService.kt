fdb1cc024accc6afb309b23b4d4aa8b0acf6002c
Ankitgujare <ankitgujare001@gmail.com>
2026-08-12 22:04:37 +0530

    Added Permission

--- app/src/main/java/gaur/himanshu/gpstracker/service/LocationService.kt
+++ app/src/main/java/gaur/himanshu/gpstracker/service/LocationService.kt
@@ -1,5 +1,6 @@
 package gaur.himanshu.gpstracker.service
 
+import android.Manifest
 import android.Manifest.permission.POST_NOTIFICATIONS
 import android.app.Service
 import android.content.Intent
@@ -8,6 +9,7 @@ import android.os.Build
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
+import androidx.annotation.RequiresPermission
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -64,6 +66,7 @@ class LocationService : Service() {
         super.onDestroy()
     }
 
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
     fun updateLocation(){
         val fusedLocationClient= LocationServices.getFusedLocationProviderClient(this)
         fusedLocationClient.requestLocationUpdates(
